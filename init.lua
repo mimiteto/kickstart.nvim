8a5a8678b90eec1a0e620046585cccec223afa6f
Dimitar Ivanov <dimitar.ivanov04@sap.com>
2024-01-03 15:47:50 +0200

    Changing airline theme

--- init.lua
+++ init.lua
@@ -232,7 +232,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'codedark',
+        theme = 'onedark',
         component_separators = '|',
         section_separators = '',
       },
