b066d0ea7ffc490f24653d969a52c3151b879f76
Dimitar Ivanov <dimitar.ivanov04@sap.com>
2024-07-12 14:47:10 +0300

    Adding test cover quick command

--- ftplugin/go.lua
+++ ftplugin/go.lua
@@ -5,3 +5,18 @@ end, { desc = 'Dap [T]est [M]ethod' })
 vim.keymap.set('n', '<leader>tlm', function()
   require('dap-go').debug_last_test()
 end, { desc = 'Dap [T]est [L]ast [M]ethod' })
+
+vim.keymap.set(
+  'n', '<leader>tc', '<cmd>! go test -cover<CR>',
+  { desc = 'CLI [T]est [C]overage' }
+)
+
+vim.keymap.set(
+  'n', '<leader>tc', '<cmd>! go test -race<CR>',
+  { desc = 'CLI [T]est [R]ace' }
+)
+
+vim.keymap.set(
+  'n', '<leader>tc', '<cmd>! go test -bench=.<CR>',
+  { desc = 'CLI [T]est [B]ench' }
+)
