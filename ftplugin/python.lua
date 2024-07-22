dfd44e7deec829fdc7c047cc7b93904a9ae16536
Dimitar Ivanov <mimiteto@gmail.com>
2024-07-22 16:32:21 +0300

    Adding python debugging shortcuts

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -7,3 +7,15 @@ end, { desc = 'Dap [T]est [M]ethod' })
 vim.keymap.set('n', '<leader>tc', function()
   require('dap-python').test_class()
 end, { desc = 'Dap [T]est [C]lass' })
+
+vim.keymap.set('n', '<leader>tf', function()
+  require('dap-python').test_file()
+end, { desc = 'Dap [T]est [F]ile' })
+
+vim.keymap.set('n', '<leader>rp', function()
+  vim.cmd('!python %<CR>')
+end, { desc = '[R]un [P]rogram' })
+
+vim.keymap.set('n', '<leader>rpa', function()
+  vim.cmd('!python %')
+end, { desc = '[R]un [P]rogram with [A]rgs' })
