f748e32fd7670c1ffabdafdf6d6652e19f098736
Dimitar Ivanov <dimitar.ivanov04@sap.com>
2025-06-15 15:21:33 +0300

    Adding custom plugins

--- init.lua
+++ init.lua
@@ -853,13 +853,6 @@ require('lazy').setup({
         -- By default, you may press `<c-space>` to show the documentation.
         -- Optionally, set `auto_show = true` to show the documentation after a delay.
         documentation = { auto_show = true, auto_show_delay_ms = 500 },
-        trigger = {
-          show_on_backspace = true,
-          show_on_insert = true,
-        },
-        directory_priority = {
-          enabled = true,
-        },
       },
 
       sources = {
@@ -983,7 +976,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
