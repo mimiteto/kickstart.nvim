919c89c18184fb6a2167cb4ce1b06b6c5612649b
Dimitar Ivanov <dimitar.ivanov04@sap.com>
2025-06-15 15:47:33 +0300

    Add filetype reconfigurations

---
+++ lua/custom/configs/filetype_reconfigure.lua
@@ -0,0 +1,25 @@
+local autocmd = vim.api.nvim_create_autocmd
+
+-- Reconfigures file matched by pattern to specified filetype
+-- @param pattern string
+-- @param filetype string
+-- @return integer
+local function reconfigure_filetype(pattern, filetype)
+  return autocmd({
+    'BufNewFile',
+    'BufRead',
+  }, {
+    pattern = { pattern },
+    callback = function()
+      vim.bo.filetype = filetype
+    end,
+  })
+end
+
+-- Reconfigure the following MacOS only
+if vim.fn.has 'mac' == 1 then
+  reconfigure_filetype('pipeline_definitions', 'yaml')
+  reconfigure_filetype('branch.cfg', 'yaml')
+  reconfigure_filetype('config', 'yaml')
+  reconfigure_filetype('processing.cfg', 'yaml')
+end
