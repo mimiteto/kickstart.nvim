38c29eeb9c1de17f3f25694dd61554c2e885f250
Dimitar Ivanov <dimitar.ivanov04@sap.com>
2025-06-16 11:00:50 +0300

    Optimal `spider` configuration

--- lua/custom/plugins/golang.lua
+++ lua/custom/plugins/golang.lua
@@ -1,12 +1,15 @@
 return {
   'chrisgrieser/nvim-spider',
   lazy = true,
-  opts = {
-    skipInsignificantPunctuation = false,
-  },
-  keys = {
-    { 'w', "<cmd>lua require('spider').motion('w')<CR>", mode = { 'n', 'o', 'x' } },
-    { 'e', "<cmd>lua require('spider').motion('e')<CR>", mode = { 'n', 'o', 'x' } },
-    { 'b', "<cmd>lua require('spider').motion('b')<CR>", mode = { 'n', 'o', 'x' } },
-  },
+  ft = 'go',
+  config = function()
+    require('spider').setup {
+      skipInsignificantPunctuation = false,
+    }
+
+    -- Set up keymaps immediately since we're already in a Go buffer
+    vim.keymap.set({ 'n', 'o', 'x' }, 'w', "<cmd>lua require('spider').motion('w')<CR>", { buffer = true })
+    vim.keymap.set({ 'n', 'o', 'x' }, 'e', "<cmd>lua require('spider').motion('e')<CR>", { buffer = true })
+    vim.keymap.set({ 'n', 'o', 'x' }, 'b', "<cmd>lua require('spider').motion('b')<CR>", { buffer = true })
+  end,
 }
