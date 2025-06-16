0a293157e3fcc40760d473dd4bff679eae3f9b40
Dimitar Ivanov <dimitar.ivanov04@sap.com>
2025-06-16 17:39:15 +0300

    Adding cursor config

---
+++ lua/custom/configs/cursor.lua
@@ -0,0 +1,15 @@
+-- Exiting with a blinking cursor
+
+local M = {}
+
+function M.setup()
+  vim.api.nvim_create_autocmd('VimLeave', {
+    once = true,
+    callback = function()
+      vim.opt.guicursor =
+        'n-v-c-i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175'
+    end,
+  })
+end
+
+return M
