7710dd9ee4b65936de2880ba43963e937d2dfc35
Dimitar Ivanov <dimitar.ivanov04@sap.com>
2025-06-17 09:52:15 +0300

    Improving handling for cursos

--- lua/custom/configs/cursor.lua
+++ lua/custom/configs/cursor.lua
@@ -3,6 +3,8 @@
 local M = {}
 
 function M.setup()
+  vim.opt.guicursor =
+    'n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175'
   vim.api.nvim_create_autocmd('VimLeave', {
     once = true,
     callback = function()
