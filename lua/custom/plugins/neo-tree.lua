9e0d3d8b15c3ed8dfc719af26834aa71925ef053
Dimitar Ivanov <dimitar.ivanov04@sap.com>
2025-07-16 09:32:57 +0300

    Chaning the configuration of neo-tree

---
+++ lua/custom/plugins/neo-tree.lua
@@ -0,0 +1,60 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+    {
+      's1n7ax/nvim-window-picker', -- for open_with_window_picker keymaps
+      version = '2.*',
+      config = function()
+        require('window-picker').setup {
+          filter_rules = {
+            include_current_win = false,
+            autoselect_one = true,
+            -- filter using buffer options
+            bo = {
+              -- if the file type is one of following, the window will be ignored
+              filetype = { 'neo-tree', 'neo-tree-popup', 'notify' },
+              -- if the buffer type is one of following, the window will be ignored
+              buftype = { 'terminal', 'quickfix' },
+            },
+          },
+        }
+      end,
+    },
+  },
+  lazy = false,
+  keys = {
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  opts = {
+    filesystem = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+          ['y'] = function(state)
+            -- NeoTree is based on [NuiTree](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree)
+            -- The node is based on [NuiNode](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree#nuitreenode)
+            local node = state.tree:get_node()
+            local filename = node.name
+            vim.fn.setreg('"', filename)
+            vim.notify('Copied: ' .. filename)
+          end,
+          ['Y'] = function(state)
+            -- NeoTree is based on [NuiTree](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree)
+            -- The node is based on [NuiNode](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree#nuitreenode)
+            local node = state.tree:get_node()
+            local filepath = node:get_id()
+            vim.fn.setreg('"', filepath)
+            vim.notify('Copied: ' .. filepath)
+          end,
+        },
+      },
+    },
+  },
+}
