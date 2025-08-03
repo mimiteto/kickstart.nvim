ff3f00852b32deb0f3fe077dcf134033ce92f5e5
Dimitar Ivanov <mimiteto@gmail.com>
2025-08-03 14:52:02 +0300

    neo-tree shows hidden files by default

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -34,6 +34,9 @@ return {
   },
   opts = {
     filesystem = {
+      filtered_items = {
+        visible = true,
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
