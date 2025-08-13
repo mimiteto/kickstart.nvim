fc8c339750f5b954061e977b692e2ab32dd2361e
Dimitar Ivanov <dimitar.ivanov04@sap.com>
2025-08-13 18:10:25 +0300

    Lualine instead of statusline

---
+++ lua/custom/plugins/lualine.lua
@@ -0,0 +1,35 @@
+local function current_symbol()
+  local trouble = require 'trouble'
+  local symbols = trouble.statusline {
+    mode = 'lsp_document_symbols',
+    groups = {},
+    title = false,
+    filter = { range = true },
+    format = '{kind_icon}{symbol.name:Normal}',
+    hl_group = 'lualine_c_normal',
+  }
+  return symbols.get()
+end
+
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons', 'folke/trouble.nvim' },
+  config = function()
+    require('lualine').setup {
+      extensions = { 'trouble', 'neo-tree', 'quickfix' },
+      always_divide_middle = false,
+      sections = {
+        lualine_a = {
+          {
+            'mode',
+            fmt = function(str)
+              return str:sub(1, 1)
+            end,
+          },
+        },
+        lualine_c = { 'filename', 'lsp_status', current_symbol },
+        lualine_y = { 'progress', 'searchcount' },
+      },
+    }
+  end,
+}
