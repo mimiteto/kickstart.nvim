763d8724dbb2dce88b5428ecfc299885ef618474
Dimitar Ivanov <dimitar.ivanov04@sap.com>
2026-04-08 14:25:00 +0300

    Correcting noice.

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -1,26 +1,22 @@
 return {
   'folke/noice.nvim',
   event = 'VeryLazy',
-  setup = function()
-    require('noice').setup {
-      lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-          ['vim.lsp.util.stylize_markdown'] = true,
-          ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
-        },
+  opts = {
+    lsp = {
+      override = {
+        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+        -- stylize_markdown is deprecated in nvim 0.12, noice handles this internally now
+        ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
       },
-      -- you can enable a preset for easier configuration
-      presets = {
-        bottom_search = true, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = true, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = true, -- add a border to hover docs and signature help
-      },
-    }
-  end,
+    },
+    presets = {
+      bottom_search = true, -- use a classic bottom cmdline for search
+      command_palette = true, -- position the cmdline and popupmenu together
+      long_message_to_split = true, -- long messages will be sent to a split
+      inc_rename = true, -- enables an input dialog for inc-rename.nvim
+      lsp_doc_border = true, -- add a border to hover docs and signature help
+    },
+  },
   dependencies = {
     'MunifTanjim/nui.nvim',
     'rcarriga/nvim-notify',
